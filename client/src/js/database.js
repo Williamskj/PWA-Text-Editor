2f6ca841c1209ec16b462e8910ef5f487261771f
Williamskj <89874192+Williamskj@users.noreply.github.com>
2022-02-18 10:39:09 -0800

    Add files via upload

---
+++ client/src/js/database.js
@@ -0,0 +1,35 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jateDB = await openDB('jate', 1)
+  const tx = jateDB.transaction('jate', 'readwrite')
+  const store = tx.objectStore('jate')
+  const request = store.add({ text: content })
+  const result = await request
+  console.log(result)
+};
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDB = await openDB('jate', 1)
+  const tx = jateDB.transaction('jate', 'readonly')
+  const store = tx.objectStore('jate')
+  const request = store.getAll()
+  const result = await request 
+  console.log('result.value', result)
+  return result[0].content
+}
+
+initdb();
